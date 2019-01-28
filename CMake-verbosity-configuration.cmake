69e17aaaa5be539d7e35a8fead968cc947e2cfed
Jiteman <jiteman@gmail.com>
2019-01-28 11:09:26 +0300

    FCB's revision has been updated (test for cppunit)

--- CMake-verbosity-configuration.cmake
+++ CMake-verbosity-configuration.cmake
@@ -1,4 +1,6 @@
-set( JMSD_CMAKE_VERBOSE_OUTPUT is_on ) # [ is_on | is_off ]
+#set( JMSD_CMAKE_PROJECT_NAME_OUTPUT is_off ) # [ is_on | is_off ]
+set( JMSD_CMAKE_PROJECT_NAME_OUTPUT is_on ) # [ is_on | is_off ]
 #set( JMSD_CMAKE_VERBOSE_OUTPUT is_off ) # [ is_on | is_off ]
-set( JMSD_CMAKE_DEPENDENCY_OUTPUT is_on ) # [ is_on | is_off ]
+set( JMSD_CMAKE_VERBOSE_OUTPUT is_on ) # [ is_on | is_off ]
 #set( JMSD_CMAKE_DEPENDENCY_OUTPUT is_off ) # [ is_on | is_off ]
+set( JMSD_CMAKE_DEPENDENCY_OUTPUT is_on ) # [ is_on | is_off ]
